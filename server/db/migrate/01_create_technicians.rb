c0afeb5b129772605dd6a5d8e8c814ecfd5ee941
Calvin <calvinkioko91@gmail.com>
2022-09-08 13:46:55 +0300

    Creating technicians table for database

---
+++ server/db/migrate/01_create_technicians.rb
@@ -0,0 +1,15 @@
+class CreateTechnicians < ActiveRecord::Migration[6.1]
+  def change
+    #create technicians table
+    create_table :technicians do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :town
+      t.string :job_type
+      t.string :job_description
+    end
+
+  end
+end
