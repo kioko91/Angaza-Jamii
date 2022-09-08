e0d25823486f3737bfc413118a944cca4f944f2b
Calvin <calvinkioko91@gmail.com>
2022-09-08 14:20:22 +0300

    Created welcome message for  back-end application

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+     "Welcome to Angaza Jamii Back-end Application!💡" 
   end
 
+  
 end
