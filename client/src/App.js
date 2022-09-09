ee9870ca34b176efecab8148d23deecdfa1ec9ad
Calvin <calvinkioko91@gmail.com>
2022-09-09 08:00:08 +0300

    Final edits to the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect,useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Navigation from './components/Navigation';
+import ApplyCustomer from './components/ApplyCustomer';
+import Landing from './components/Landing';
+import Footer from './components/Footer';
+import SolarTypes from './components/SolarTypes';
+import Technicians from './components/Technicians';
+import About from './components/About';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    
+    
+    <BrowserRouter>
+    <div >
+    <Navigation />
+      <Routes>
+      <Route exact path="/" element={<Landing />} />
+      <Route path="/home" element={<Landing />} />
+      <Route exact path="/solartypes" element={<SolarTypes />} />
+      <Route exact path="/application" element={<ApplyCustomer />} />
+      <Route exact path="/technicians" element={<Technicians />} />
+      <Route exact path="/about" element={<About />} />
+      </Routes>
+    <Footer />
     </div>
+    </BrowserRouter>
+    
+    </div>
+    
+    
   );
 }
 
